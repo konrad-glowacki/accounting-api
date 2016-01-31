b5c853b7acbd32d9265f0bd5845212f5559f3939
Konrad Głowacki <konrad@webkrak.pl>
2016-01-31 22:51:16 +0100

    Add route homepage

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+  res.status(200).send('Welcome!');
+});
+
 router.use('/api/accountants', require('./accountants'));
 router.use('/api/customers', require('./customers'));
 
