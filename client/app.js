9778ba08233748a4d380e9c1beaca18a23ec7c92
Konrad Głowacki <konrad@webkrak.pl>
2016-02-25 23:26:23 +0100

    Remove libraries from repo

--- client/app.js
+++ client/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/components', express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req, res){
   res.redirect('/index.html');
