5f164462fce365e01dfe3d3f6f0591517c072619
Konrad Głowacki <konrad@webkrak.pl>
2016-04-08 21:55:37 +0200

    Replace bcrypt to bcryptjs

--- server/models/accountant.js
+++ server/models/accountant.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 const emailRegex = require('../lib/email_regex');
