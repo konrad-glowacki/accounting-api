fe3473de5d8caeafef2a1867c837e28b4073eb5e
Konrad Głowacki <konrad@webkrak.pl>
2016-04-11 23:55:54 +0200

    Coding improvements

--- client/app.js
+++ client/app.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var logger = require('morgan');
-var path = require('path');
-var app = express();
+'use strict';
+
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/components', express.static(__dirname + '/bower_components'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.redirect('/index.html');
 });
 
